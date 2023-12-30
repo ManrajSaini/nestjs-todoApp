3a60f6b50734b143f3c6165da4a725b891a8197b
Manraj Singh <manraj.saini123@gmail.com>
2023-12-30 12:10:04 +0530

    added todo services

---
+++ src/todo/services/todo/todo.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from 'src/todo/entities/todo/todo';
+import { Repository } from 'typeorm';
+import { TodoMapperService } from '../todo-mapper/todo-mapper.service';
+import { TodoDto } from 'src/todo/dto/todo.dto';
+import { AddTodoDto } from 'src/todo/dto/add-todo.dto';
+import { EditTodoDto } from 'src/todo/dto/edit-todo.dto';
+
+@Injectable()
+export class TodoService {
+
+    public constructor(
+        @InjectRepository(Todo) 
+        private readonly todoRepository: Repository<Todo>,
+        private readonly todoMapper: TodoMapperService
+    ){}
+
+    public async findAll(): Promise<TodoDto[]> {
+        const todos = await this.todoRepository.find();
+        return todos.map(this.todoMapper.modelToDto);
+    }
+
+    public async findOne(id: number): Promise<TodoDto> {
+        const todo = await this.todoRepository.findOne(id);
+
+        if(todo == null) throw new NotFoundException();
+
+        return this.todoMapper.modelToDto(todo);
+    }
+
+    public async add({ title }: AddTodoDto): Promise<TodoDto> {
+        let todo = new Todo(title);
+        todo = await this.todoRepository.save(todo);
+
+        return this.todoMapper.modelToDto(todo);
+    }
+
+    public async edit(id: number, { title, completed }: EditTodoDto): Promise<TodoDto> {
+        let todo = await this.todoRepository.findOne(id);
+
+        if(todo == null) throw new NotFoundException();
+
+        todo.completed = completed;
+        todo.title = title;
+
+        todo = await this.todoRepository.save(todo);
+
+        return this.todoMapper.modelToDto(todo);
+    }
+
+    public async remove(id: number): Promise<Todo> {
+        let todo = await this.todoRepository.findOne(id);
+
+        if(todo == null) throw new NotFoundException();
+
+        todo = await this.todoRepository.remove(todo);
+
+        return todo;
+    }
+}
--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './entities/todo/todo';
+import { TodoService } from './services/todo/todo.service';
+import { TodoMapperService } from './services/todo-mapper/todo-mapper.service';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Todo])
     ],
-    providers: [],
+    providers: [TodoService, TodoMapperService],
     controllers: []
 })
 export class TodoModule {}
