8e08579f5cd9a619bb54c4ee913cab5f76029905
Manraj Singh <manraj.saini123@gmail.com>
2023-12-31 11:11:48 +0530

    data valodation done

--- src/todo/dto/edit-todo.dto.ts
+++ src/todo/dto/edit-todo.dto.ts
@@ -1,6 +1,12 @@
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
 export class EditTodoDto {
-    
+
+    @IsString()
+    @IsNotEmpty()
     public readonly title: string;
+
+    @IsBoolean()
     public readonly completed: boolean;
 
     public constructor(opts?: Partial<EditTodoDto>) {
